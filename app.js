2cf2786af27607d639407fd49814d3fe1d038f16
ludexab <ileanwaabalaka@gmail.com>
2022-09-11 05:04:02 +0100

    minor update

---
+++ app.js
@@ -0,0 +1,18 @@
+import { Express } from "express";
+import { appendFile } from "fs";
+import path from "path";
+
+const App = Express();
+
+if (process.env.NODE_ENV === "production") {
+  App.use(Express.static("dist"));
+  App.get("*", (req, res) => {
+    req.sendFile(path.resolve(__dirname, "dist", "index.html"));
+  });
+}
+
+const port = process.env.port || 5173;
+App.listing(prot, (err) => {
+  if (err) return console.log(err);
+  console.log("server running on port: ", port);
+});
